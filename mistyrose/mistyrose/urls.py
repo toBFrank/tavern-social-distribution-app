cfadceeea6dab3e1af24a1ba1ab0c38603afbab4
Rithwik Korukonda Bhattar <korukond@ualberta.ca>
2024-10-15 11:44:50 -0600

    changed name

--- mistyrose/mistyrose/urls.py
+++ mistyrose/mistyrose/urls.py
@@ -34,5 +34,5 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger"),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name="swagger"),
 ]
