fc5343038887983d9121d97743c09c7431d77472
Rithwik Korukonda Bhattar <korukond@ualberta.ca>
2024-10-16 10:08:14 -0600

    commit

--- mistyrose/node/admin.py
+++ mistyrose/node/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Node
+class NodeAdmin(admin.ModelAdmin):
+    model = Node
+    list_display = ('host', 'username', 'password', 'team')
+    list_display_links = None
+    list_editable = ('host', 'username', 'password', 'team')
+    list_filter = ('host',)
+    search_fields = ('host', 'username')
+    ordering = ('host',)
+    
+admin.site.register(Node)
--- mistyrose/node/views.py
+++ mistyrose/node/views.py
@@ -1,13 +1,19 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import AllowAny
-from rest_framework import status
+from rest_framework.decorators import renderer_classes, permission_classes
 from .models import Node
 from .serializers import NodeSerializer
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import get_object_or_404
-from base64 import b64decode
+from rest_framework.views import APIView
 
-# Create your views here.
+class NodeList(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        nodes = Node.objects.all().filter(isActive = True)
+        serializer = NodeSerializer(nodes, many=True)
+        response = {
+            "type": "nodes",
+            "items": serializer.data,
+        }
+        return Response(response, status=status.HTTP_200_OK)
