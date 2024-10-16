3be040e32a54d986f5a6311f949a05b14f178638
Franco <bonilla.franco484@gmail.com>
2024-10-16 15:33:29 -0600

    posts views progress.

--- mistyrose/posts/urls.py
+++ mistyrose/posts/urls.py
@@ -1,12 +1,10 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, LikeViewSet
-
-router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'comments', CommentViewSet)
-router.register(r'likes', LikeViewSet)
+from django.urls import path
+from .views import AllPostsView, AuthorPostsView, PostDetailsByFqidView, PostDetailsView, PostImageView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('posts/', AllPostsView.as_view(), name='post-list'),
+    path('authors/<uuid:author_serial>/posts/<uuid:post_serial>/', PostDetailsView.as_view(), name='post-detail'),
+    path('posts/<uuid:post_fqid>/', PostDetailsByFqidView.as_view(), name='post-detail-fqid'),
+    path('authors/<uuid:author_serial>/posts/', AuthorPostsView.as_view(), name='author-posts'),
+    path('authors/<uuid:author_serial>/posts/<uuid:post_serial>/image/', PostImageView.as_view(), name='post-image'),
 ]
