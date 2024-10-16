781671d380aef792c61b8d145023e260397be877
Rithwik Korukonda Bhattar <korukond@ualberta.ca>
2024-10-16 14:27:29 -0600

    Deleting unnecessary code

--- mistyrose/node/admin.py
+++ mistyrose/node/admin.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
 
 from .models import Node
-class NodeAdmin(admin.ModelAdmin):
-    model = Node
-    list_display = ('host', 'username', 'password', 'team')
-    list_display_links = None
-    list_editable = ('host', 'username', 'password', 'team')
-    list_filter = ('host',)
-    search_fields = ('host', 'username')
-    ordering = ('host',)
     
 admin.site.register(Node)
