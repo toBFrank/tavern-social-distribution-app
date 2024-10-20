5668c5102c47130be0af53fb47ad84b74893a7b7
Franco <bonilla.franco484@gmail.com>
2024-10-20 01:33:30 -0600

    Global input styling.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
 
-import "../styles/pages/Home.css"
+import '../styles/pages/Home.css';
 import FollowRequests from '../components/FollowRequests';
-import SearchBar from "../components/SearchBar";
-import SearchResultsList from "../components/SearchResultsList";
+import SearchBar from '../components/SearchBar';
+import SearchResultsList from '../components/SearchResultsList';
 
 const Home = () => {
   const [results, setResults] = useState([]);
 
   return (
     <div className="home-container">
+      <h1>Home</h1>
       <div className="main-container">
         <SearchBar setResults={setResults} />
-        {results && results.length > 0 && <SearchResultsList results={results} />}
-        <h1>Home</h1>
+        {results && results.length > 0 && (
+          <SearchResultsList results={results} />
+        )}
       </div>
-      <div className='follow-request-container'>
+      <div className="follow-request-container">
         <FollowRequests />
       </div>
     </div>
