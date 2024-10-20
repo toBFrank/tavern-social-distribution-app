4b4332331bdfa363d0a2290d076ea5e0abd9181e
Xiangxu Meng <xiangxu@ualberta.ca>
2024-10-20 05:02:50 -0600

    accomplish accept or deny follow request
    accomplish accept or deny follow request

--- frontend/src/services/FollowService.js
+++ frontend/src/services/FollowService.js
@@ -11,3 +11,29 @@ export const getFollowRequests = async (authorSerial) => {
       console.error(error);
     }
   };
+
+// URL: service/api/authors/<str:author_id>/followers/<str:follower_id>/
+export const acceptFollowRequest = async (authorId, followerId) => {
+  try {
+      const response = await api.put(`http://localhost:8000/service/api/authors/${authorId}/followers/${followerId}/`);  // 使用绝对 URL
+      return response.data;
+  } catch (error) {
+      console.error("Error accepting follow request:", error);
+  }
+};
+
+export const rejectFollowRequest = async (authorId, followerId) => {
+  try {
+      await api.delete(`http://localhost:8000/service/api/authors/${authorId}/followers/${followerId}/`);  // 使用绝对 URL
+  } catch (error) {
+      console.error("Error rejecting follow request:", error);
+  }
+};
+
+
+
+
+
+
+
+
