ed539deb301623f4659af54ee95508b3dc84b993
Franco <bonilla.franco484@gmail.com>
2024-11-04 09:43:22 -0700

    post urls

--- mistyrose/posts/urls.py
+++ mistyrose/posts/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import AuthorPostsView, PostDetailsByFqidView, PostDetailsView, PostImageView, CommentsByFQIDView, PublicPostsView, LikesViewByFQIDView
+from .views import AuthorPostsView, PostDetailsByFqidView, PostImageView, CommentsByFQIDView, PublicPostsView, LikesViewByFQIDView
 urlpatterns = [
     # Post URLs
-    path('authors/<uuid:author_serial>/posts/<uuid:post_serial>/', PostDetailsView.as_view(), name='post-detail'),
     path('<uuid:post_fqid>/', PostDetailsByFqidView.as_view(), name='post-detail-fqid'),
-    path('authors/<uuid:author_serial>/posts/', AuthorPostsView.as_view(), name='author-posts'),
-    path('authors/<uuid:author_serial>/posts/<uuid:post_serial>/image/', PostImageView.as_view(), name='post-image'),
     path('', PublicPostsView.as_view(), name='public-posts'),  
     
     # Comment URLs
