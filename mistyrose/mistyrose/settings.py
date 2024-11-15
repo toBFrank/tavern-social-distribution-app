c348967829535b2d720e76589e2f3f36afb89e84
SyjermelAsuncion <syjermelasuncion@gmail.com>
2024-11-15 11:28:41 -0700

    Update Procfile and settings.py

--- mistyrose/mistyrose/settings.py
+++ mistyrose/mistyrose/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from datetime import timedelta
-from pathlib import Path
 import os
 import dj_database_url
+from datetime import timedelta
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,6 +115,7 @@ else:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
