f2fb3ddd884ccdb7bdcdd6745a5b32cfd79e4020
kelly-shih <kshih@ualberta.ca>
2024-11-16 02:33:18 -0700

    undo commit

--- mistyrose/mistyrose/settings.py
+++ mistyrose/mistyrose/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = 'mistyrose.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR.parent, 'frontend/public')],
+        'DIRS': [os.path.join(BASE_DIR.parent, 'frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
