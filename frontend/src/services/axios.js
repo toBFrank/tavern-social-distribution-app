e3c911ca57817a581e6df3f42b31094d7dabb3a6
Rithwik Korukonda Bhattar <korukond@ualberta.ca>
2024-11-18 05:30:40 -0700

    commit

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: `${window.location.origin}/api/`,
+  baseURL: `localhost:8000/api/`,
   withCredentials: true,
 });
 
