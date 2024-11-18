826f99e8ff2a50e30ba623222ce8d62037c9adbc
Rithwik Korukonda Bhattar <korukond@ualberta.ca>
2024-11-18 11:08:35 -0700

    not needed

--- mistyrose/node/utils.py
+++ mistyrose/node/utils.py
@@ -14,18 +14,3 @@ def fetch_remote_data(node, endpoint):
         return response.json()  # Return the JSON response
     except requests.exceptions.RequestException as e:
         raise Exception(f"Error connecting to remote node: {str(e)}")
-    
-def send_data_to_remote(node, endpoint, data):
-    """
-    Send data to a remote node via POST request.
-    """
-    try:
-        response = requests.post(
-            f"{node.host}{endpoint}",
-            json=data,  # Send the data as JSON in the request body
-            auth=HTTPBasicAuth(node.username, node.password)
-        )
-        response.raise_for_status()  # Raise exception for non-200 responses
-        return response.json()  # Return the JSON response
-    except requests.exceptions.RequestException as e:
-        raise Exception(f"Error connecting to remote node: {str(e)}")
