3743fff685270eb0acec5913abc5701cd3473c78
Franco <bonilla.franco484@gmail.com>
2024-11-23 11:11:03 -0700

    urls fix?

--- mistyrose/posts/authors_urls.py
+++ mistyrose/posts/authors_urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import (
     AuthorPostsView,
     CommentedView, 
@@ -17,9 +17,7 @@ from .views import (
 """
 for post urls that start with api/authors/
 """
-urlpatterns = [
-    path('api/authors/', include('users.authors_urls')), #api/authors/ for urls like following and authors
-    
+urlpatterns = [    
     # Post URLs
     path('<uuid:author_serial>/posts/<uuid:post_serial>/', PostDetailsView.as_view(), name='post-detail'),
     path('<uuid:author_serial>/posts/', AuthorPostsView.as_view(), name='author-posts'),
