7730b1e840640cc6f31f30108d262b4e6b0b46e0
Franco <bonilla.franco484@gmail.com>
2024-11-23 13:04:13 -0700

    url fix

--- mistyrose/mistyrose/urls.py
+++ mistyrose/mistyrose/urls.py
@@ -44,7 +44,7 @@ urlpatterns = [
     path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name="swagger"),
     path('', include('users.urls')),
     path('api/authors/', include('posts.authors_urls')), #api/authors/ urls for posts, likes, comments
-    re_path(r'^api/authors/?', include('users.authors_urls')), #api/authors/ for urls like following and authors
+    re_path(r'^api/authors/', include('users.authors_urls')), #api/authors/ for urls like following and authors
     path('api/liked/', include('posts.liked_urls')), #api/liked urls
     path('api/comment/', include('posts.comment_urls')), #api/comment urls
     path('api/commented/', include('posts.comment_urls')), #TODO: asked if there is an error in the project description, is this supposed to be the same one as the comments/comment_fqid?  
