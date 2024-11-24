a8bf4484fa8332f77aebbe2b4beb9877885d4411
Franco <bonilla.franco484@gmail.com>
2024-11-23 21:35:54 -0700

    author fqid

--- mistyrose/users/urls.py
+++ mistyrose/users/urls.py
@@ -21,13 +21,18 @@ urlpatterns = [
     path('api/signup/', SignUpView.as_view(), name='signup'),  # User signup endpoint
     path('api/authors/<path:pk>/', AuthorDetailView.as_view(), name='author-detail'),  # Author detail view
     path('logout/', LogoutView.as_view(), name='logout'),  # User logout endpoint
-    path('api/authors/<uuid:pk>/profile/', AuthorProfileView.as_view(), name='author-profile'),  # Author profile view
-    path('api/authors/<path:pk>/profile/', AuthorProfileView.as_view(), name='author-profile'),  # Author profile view
-    path('api/authors/<path:pk>/profile/', AuthorProfileView.as_view(), name='author-profile'),  # Author profile view
-    path('api/authors/<uuid:pk>/profile/edit/', AuthorEditProfileView.as_view(), name='author-edit-profile'),  # Edit author profile
-    path('api/authors/<path:pk>/profile/edit/', AuthorEditProfileView.as_view(), name='author-edit-profile'),  # Edit author profile
+   
     path('authors/<str:username>/upload_image/', ProfileImageUploadView.as_view(), name='upload-profile-image'),
-    path('api/authors/<path:pk>/followers/', FollowersDetailView.as_view(), name='author-followers'),  # Followers endpoint
-    path('api/authors/<path:pk>/friends/', FriendsView.as_view(), name='author-friends'),  # Friends endpoint
-    path('api/authors/<path:pk>/following/', FollowingDetailView.as_view(), name='author-following'),  # Friends endpoint
+   
+    path('api/authors/<uuid:pk>/profile/', AuthorProfileView.as_view(), name='author-profile'),  # Author profile view
+    path('api/authors/<uuid:pk>/profile/edit/', AuthorEditProfileView.as_view(), name='author-edit-profile'),  # Edit author profile
+    path('api/authors/<uuid:pk>/followers/', FollowersDetailView.as_view(), name='author-followers'),  # Followers endpoint
+    path('api/authors/<uuid:pk>/friends/', FriendsView.as_view(), name='author-friends'),  # Friends endpoint
+    path('api/authors/<uuid:pk>/following/', FollowingDetailView.as_view(), name='author-following'),  # Friends endpoint
+   
+    path('api/authors/<path:pk>/profile/', AuthorProfileView.as_view(), name='author-profile-fqid'),  # Author profile view
+    path('api/authors/<path:pk>/profile/edit/', AuthorEditProfileView.as_view(), name='author-edit-profile-fqid'),  # Edit author profile
+    path('api/authors/<path:pk>/followers/', FollowersDetailView.as_view(), name='author-followers-fqid'),  # Followers endpoint
+    path('api/authors/<path:pk>/friends/', FriendsView.as_view(), name='author-friends-fqid'),  # Friends endpoint
+    path('api/authors/<path:pk>/following/', FollowingDetailView.as_view(), name='author-following-fqid'),  # Friends endpoint
 ]
