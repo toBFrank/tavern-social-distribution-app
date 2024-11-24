282db6bd355c32163e45b1dcd893f57562f5ccd0
SyjermelAsuncion <syjermelasuncion@gmail.com>
2024-11-23 19:50:32 -0700

    Save local changes before pulling from Heroku

--- mistyrose/users/admin.py
+++ mistyrose/users/admin.py
@@ -4,6 +4,9 @@ from .models import Author, Follows
 class AuthorAdmin(admin.ModelAdmin):
     readonly_fields = ('url',) #show url in django admin
 
+class FollowsAdmin(admin.ModelAdmin):
+    list_display = ('local_follower_id', 'remote_follower_url', 'followed_id', 'status', 'is_remote')
+
 # Register your models here.
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Follows)
+admin.site.register(Follows, FollowsAdmin)
