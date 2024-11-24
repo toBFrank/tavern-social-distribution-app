d9026b341258090e1da11abbd49244b00c8343cd
Franco <bonilla.franco484@gmail.com>
2024-11-23 23:14:10 -0700

    post urls

--- mistyrose/posts/authors_urls.py
+++ mistyrose/posts/authors_urls.py
@@ -38,20 +38,20 @@ urlpatterns = [
     
     
     
-    path('<path:author_id>/posts/<path:post_id>/comments/<path:comment_id>/likes/', LikedCommentsView.as_view(), name='comment_likes'),
-    path('<path:author_serial>/post/<path:post_serial>/comment/<path:comment_fqid>/', CommentRemoteByFQIDView.as_view(), name='get_remote_comment_fqid'),
+    # path('<path:author_id>/posts/<path:post_id>/comments/<path:comment_id>/likes/', LikedCommentsView.as_view(), name='comment_likes'),
+    # path('<path:author_serial>/post/<path:post_serial>/comment/<path:comment_fqid>/', CommentRemoteByFQIDView.as_view(), name='get_remote_comment_fqid'),
     
-    path('<path:author_serial>/posts/<path:post_serial>/image/', PostImageView.as_view(), name='post-image'),
-    path('<path:author_serial>/posts/<path:post_serial>/comments/', CommentsView.as_view(), name='get_post_comments'),
-    path('<path:author_serial>/posts/<path:post_id>/likes/', LikesView.as_view(), name='post_likes'),
+    # path('<path:author_serial>/posts/<path:post_serial>/image/', PostImageView.as_view(), name='post-image'),
+    # path('<path:author_serial>/posts/<path:post_serial>/comments/', CommentsView.as_view(), name='get_post_comments'),
+    # path('<path:author_serial>/posts/<path:post_id>/likes/', LikesView.as_view(), name='post_likes'),
     
-    path('<path:author_serial>/posts/<path:post_serial>/', PostDetailsView.as_view(), name='post-detail'),
-    path('<path:author_serial>/commented/<path:comment_serial>/', CommentView.as_view(), name='comment'),
-    path('<path:author_serial>/liked/<path:like_serial>/', LikeView.as_view(), name='like'),
+    # path('<path:author_serial>/posts/<path:post_serial>/', PostDetailsView.as_view(), name='post-detail'),
+    # path('<path:author_serial>/commented/<path:comment_serial>/', CommentView.as_view(), name='comment'),
+    # path('<path:author_serial>/liked/<path:like_serial>/', LikeView.as_view(), name='like'),
     
-    path('<path:author_serial>/posts/', AuthorPostsView.as_view(), name='author-posts'),
-    path('<path:author_fqid>/commented/', CommentsByAuthorFQIDView.as_view(), name='comments_by_author_fqid'),
-    path('<path:author_fqid>/liked/', LikedFQIDView.as_view(), name='liked_fqid'),
+    # path('<path:author_serial>/posts/', AuthorPostsView.as_view(), name='author-posts'),
+    # path('<path:author_fqid>/commented/', CommentsByAuthorFQIDView.as_view(), name='comments_by_author_fqid'),
+    # path('<path:author_fqid>/liked/', LikedFQIDView.as_view(), name='liked_fqid'),
 ]
 
 # urlpatterns = [    
