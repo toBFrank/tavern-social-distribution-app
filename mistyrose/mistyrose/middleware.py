2ed6a1da84f4fa2ffaffd4eb82c0f75ddfc9d763
sdkings <sapanwalia5@gmail.com>
2024-11-26 23:35:46 -0700

    changes

--- mistyrose/mistyrose/middleware.py
+++ mistyrose/mistyrose/middleware.py
@@ -4,7 +4,7 @@ from django.utils.deprecation import MiddlewareMixin
 class TrailingSlashMiddleware(MiddlewareMixin):
     def process_request(self, request):
         # Check if the URL doesn't end with a slash and is not an admin page
-        if not request.path.endswith('/') and not request.path.startswith('/inbox'):
+        if not request.path.endswith('/') and not request.path.endswith('/inbox'):
             # Always redirect to the URL with a trailing slash
             # Use 308 to preserve the original HTTP method (POST, PUT, etc.)
             return HttpResponsePermanentRedirect(request.path + '/', status=308)
